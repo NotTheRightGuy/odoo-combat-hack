35c062e95ce6cc4455c89aa5417513f328d5070a
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-07-14 13:25:47 +0530

    book details completed except table, working on librarian Analytics

--- src/app/(protected)/librarian/analytics/page.tsx
+++ src/app/(protected)/librarian/analytics/page.tsx
@@ -1,5 +1,44 @@
+// "use client";
+// import { useEffect } from "react"
+// import Box from '@mui/material/Box';
+// import { BarChart } from '@mui/x-charts/BarChart';
+
+// export default function UserAnalytics({ params }: { params: { userId: string } }) {
+
+//     useEffect(() => {
+//         fetch("http://localhost:3000/api/userAnalytics/" + params.userId)
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data)
+//             })
+//             .catch((err) => {
+//                 console.log(err)
+//             })
+//     }, [params.userId])
+
+//     const sample = [1, 10, 30, 50, 70, 90, 100];
+
+//     return (
+//         <Box sx={{ width: '100%', maxWidth: 500, display: 'flex', fontSize : 20 }}>
+//             <BarChart
+//                 series={[
+//                     { data: [3, 4, 1, 6, 5], label: 'Series A1' },
+//                 ]}
+//                 width={600}
+//                 height={350}
+//                 borderRadius={16}
+//                 // xAxis={[{ scaleType: 'band'}]}
+//                 yAxis={[{ scaleType: 'linear', tickLabelStyle : {fontSize: 15, } }]}
+//                 // xAxis={[{ scaleType: 'band' }]}
+
+//             />
+//         </Box>
+//     );
+// }
 "use client"
-import React, { useEffect } from "react";
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+"use client"
 import {
     Tooltip,
     TooltipContent,
@@ -29,79 +68,36 @@ import Link from "next/link";
 import { Books } from "@/types/index";
 import { Button } from "@/components/ui/button";
 
-export default function BookDetails({ params }: { params: { isbn: number } }) {
-    const [book, setBook] = React.useState<Books>();
-    // const [crimeHistory,setCrimeHistory]=useState<any[]>([])
-    // const [temp, setTemp] = useState<any[]>([]);
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+    'Page A',
+    'Page B',
+    'Page C',
+    'Page D',
+    'Page E',
+    'Page F',
+    'Page G',
+];
 
-    const getBookHistory = (id: number) => {
-        fetch("http://localhost:3000/api/books/history/" + id)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            })
-            .catch((err) => console.error(err));
-    }
-
-
-    useEffect(() => {
-        // fetch("http://localhost:3000/api/books/search")
-        fetch("http://localhost:3000/api/books?isbn=" + params.isbn)
-            .then((res) => res.json())
-            .then((data) => {
-                setBook(data[0])
-                
-                getBookHistory(data[0].id)
-                console.log(data)
-            })
-            .catch((err) => console.error(err));
-        
-
-            // fetch('/api/crimeCRUD/fetchCrimeHistory')
-            // .then((res) => res.json())
-            // .then((data) => {
-            //     setCrimeHistory(data.data);
-            //     console.log(data);
-            //     console.log(data.data)
-            // })
-            // .catch((err) => console.log(err));
+export default function UserAnalytics() {
     
-            // for(let i=0;i<crimeHistory.length;i++){
-            //     setTemp([...temp,crimeHistory[i]])
-            // }
-    }, [params.isbn]);
+    const [userData, setUserdata] = React.useState([]);
 
-    // const columns = [
-    //     { field: 'id', headerName: 'ID', width: 90 },
-    //     { field: 'C_DATE', headerName: 'Date', width: 150 },
-    //     { field: 'C_DESC', headerName: 'Description', width: 150 },
-    //     { field: 'C_LATITUDE', headerName: 'Latitude', width: 150 },
-    //     { field: 'C_LONGITUDE', headerName: 'Longitude', width: 150 },
-    //     { field: 'C_NAME', headerName: 'Name', width: 150 },
-    //     { field: 'C_STATUS', headerName: 'Status', width: 150 },
-    //     { field: 'C_TIME', headerName: 'Time', width: 150 },
-    //     { field: 'C_TYPE', headerName: 'Type', width: 150 },
-    //   ];
-
-    // const rows =
-    // crimeHistory.map((crime)=>{
-    //         return(
-    //             {
-    //                 id:crime.C_ID,
-    //                 C_DATE:crime.C_DATE,
-    //                 C_DESC:crime.C_DESC,
-    //                 C_LATITUDE:crime.C_LATITUDE,
-    //                 C_LONGITUDE:crime.C_LONGITUDE,
-    //                 C_NAME:crime.C_NAME,
-    //                 C_STATUS:crime.C_STATUS,
-    //                 C_TIME:crime.C_TIME,
-    //                 C_TYPE:crime.C_TYPE
-    //             }
-    //         )
-    //     })
+    React.useEffect (() => {
+        fetch("http://localhost:3000/api/books")
+            .then((res) => res.json())
+            .then((data) => {
+                setUserdata(data)
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     return (
-        <div className="flex w-screen">
+        <div className='w-screen flex'>
             <aside className="inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
                 <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
                     <Link
@@ -199,43 +195,23 @@ export default function BookDetails({ params }: { params: { isbn: number } }) {
                     </TooltipProvider>
                 </nav>
             </aside>
-            <div className="p-8 flex flex-col gap-8 w-full">
-                <h1 className="font-bold text-xl">Book detail & checkout </h1>
-                <div className="flex justify-between w-full">
-                    <div className="flex gap-2">
 
-                        <img src={book?.thumbnail} alt="" className="rounded-xl h-40 w-28 object-none" />
-                        <div className="flex justify-between flex-col p-2">
-                            <h1 className="font-bold text-xl">
-                                {book?.title}
-                            </h1>
-                            <div className="grid grid-cols-2 gap-3 gap-x-4 [&>*]:font-medium text-sm text-[#475579]">
-                                <p> Categories : {book?.categories[0]} </p>
-                                <p> ISBN : {book?.isbn} </p>
-                                <p> Pages : {book?.pageCount} </p>
-                                <p> Publishe Date : {book?.publishedDate} </p>
-                                <p> Authors : {book?.authors.map((author) => { return author })} </p>
-                                <p> Quantity : {book?.quantity} </p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-between">
-                        <Button className="bg-green-100 text-green-700"> Available </Button>
-                        <Button> Lend Book </Button>
-                    </div>
-                </div>
-                <div>
-                    <h1>
-                        Description
-                    </h1>
-                    <p>
-                        {book?.description}
-                    </p>
-                </div>
-
+        <BarChart
+            width={500}
+            height={300}
+            series={[
+                { data: pData },
+            ]}
+            xAxis={[{ data: xLabels,
+                // data : userData.history.date 
+                scaleType: 'band', disableTicks: true, disableLine: true }]}
+                yAxis={[{ data : pData, 
+                // data: userData.history.borrowedBooks,
+                disableLine: true, disableTicks: true, scaleType: 'linear'}]}
+            borderRadius={16}
+            disableAxisListener={true}
+            
+            />
             </div>
-
-        </div>
-    )
+    );
 }
